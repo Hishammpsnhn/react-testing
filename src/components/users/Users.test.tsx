dd241482acf280760ac6434d3913b1e473309662
hisham <hishammpsn@gmail.com>
2023-05-16 18:16:42 +0530

    msw error handler

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { Users } from "./Users"
+import { server } from "../../mocks/server"
+import { rest } from "msw"
 
 describe("users", () => {
     test("render users", () => {
@@ -7,9 +9,22 @@ describe("users", () => {
         const HeadingElement = screen.getByText('Users')
         expect(HeadingElement).toBeInTheDocument()
     })
-    test("render users ", async() => {
+    test("render users ", async () => {
         render(<Users />)
-        const users =await screen.findAllByRole('listitem')
+        const users = await screen.findAllByRole('listitem')
         expect(users).toHaveLength(3)
+    })
+    test("render errors", async () => {
+        server.use(
+            rest.get(
+                "https://jsonplaceholder.typicode.com/users",
+                (req,res,ctx)=>{
+                    return res(ctx.status(500));
+                }
+            )
+        );
+        render(<Users />)
+        const error = await screen.findByText('Error fetching users')
+        expect(error).toBeInTheDocument()
     })
 })
